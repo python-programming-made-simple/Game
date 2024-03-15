4009fb37f7b6aeb934df1720d3ad7110efe46d3d
python-programming-made-simple <160355417+python-programming-made-simple@users.noreply.github.com>
2024-03-15 16:40:06 -0400

    Delete game_object.py

--- game_object.py
+++
@@ -1,8 +0,0 @@
-class GameObject:
-    def __init__(self,window,image,x,y):
-        self.window = window
-        self.image = image
-        self.x = x
-        self.y = y
-    def draw(self):
-        self.window.blit(self.image,(self.x,self.y))
