ac24699fc25b1d32079f54293091ef6bd322b07c
python-programming-made-simple <160355417+python-programming-made-simple@users.noreply.github.com>
2024-03-15 16:42:20 -0400

    Delete score.py

--- score.py
+++
@@ -1,8 +0,0 @@
-import load
-
-def increase_score(asteroid,score_image):
-    load.asteroids.remove(asteroid)
-    load.score = load.score + 1
-    if load.score != 10:
-        load.point_sound.play()
-    score_image.display(str(load.score))
