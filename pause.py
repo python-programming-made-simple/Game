af764688f230d79cb3aa170081a074b73282489e
python-programming-made-simple <160355417+python-programming-made-simple@users.noreply.github.com>
2024-03-15 16:41:49 -0400

    Delete pause.py

--- pause.py
+++
@@ -1,8 +0,0 @@
-import booleans
-import load
-
-def pause(window):
-    load.pause_sound.play()
-    booleans.started_game = False
-    booleans.button_pressed = True
-    window.blit(load.start_png,(10,730))
